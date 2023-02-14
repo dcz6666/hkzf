6609a0dabfb82a365162c30a1417ea19115e2521
王文成 <dcz6666@126.com>
2023-02-14 23:01:26 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Button } from 'antd-mobile-v5' // v5
+
+//导入首页和城市选择两个组件
+import Home from './pages/Home'
+import CityList from './pages/CityList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+
+        <Route path="/home" element={<Home />}></Route>
+
+        <Route path="users">
+          <Route path="xxx" element={<Demo />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/CityList/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                城市选择页面
+            </div>
+        )
+    }
+}
